a6ec897733f3b0421ec8a7a0c88052046a675966
Benjamin Allen <maw@Benjamins-MacBook-Pro.local>
2022-12-02 10:24:10 +0000

    day 2

---
+++ src/aoc2022/day2.clj
@@ -0,0 +1,33 @@
+(ns aoc2022.day2 
+  (:require [clojure.string :as str]
+            [aoc2022.helpers :refer [get-input]]))
+
+(def outcomes-part-one {"A X" 4 "A Y" 8 "A Z" 3
+                        "B X" 1 "B Y" 5 "B Z" 9
+                        "C X" 7 "C Y" 2 "C Z" 6})
+
+(def outcomes-part-two {"A X" 3 "A Y" 4 "A Z" 8
+                        "B X" 1 "B Y" 5 "B Z" 9
+                        "C X" 2 "C Y" 6 "C Z" 7})
+
+(def guide (get-input "day2"))
+
+(defn total-points [guide outcomes]
+  (->> (str/split-lines guide)
+       (map outcomes)
+       (reduce +)))
+
+(defn part-one []
+  (total-points guide outcomes-part-one))
+
+(defn part-two []
+  (total-points guide outcomes-part-two))
+
+(comment
+  (def example-guide "A Y\nB X\nC Z")
+
+  (part-one)
+  (part-two)
+
+  (total-points example-guide outcomes-part-two)
+  )
