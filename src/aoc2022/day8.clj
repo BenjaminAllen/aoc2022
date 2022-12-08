4c48a295b873e07989f5311cf77f977c2f329804
Benjamin Allen <benjamin.allen@prima.it>
2022-12-08 16:21:46 +0000

    refactor

--- src/aoc2022/day8.clj
+++ src/aoc2022/day8.clj
@@ -1,15 +1,29 @@
 (ns aoc2022.day8
   (:require [clojure.string :as str]
-            [aoc2022.helpers :refer [get-input-lines count-where sum]]))
-
-(def example-input (str/split-lines "30373
-25512
-65332
-33549
-35390"))
+            [aoc2022.helpers :refer [get-input-lines count-where]]))
 
 (defn parse-line [line]
   (map #(Integer/parseInt %) (str/split line #"")))
+ 
+(def forest (->> (get-input-lines "day8")
+                 (map parse-line)))
+
+(defn rotate [forest]
+  (apply map vector forest))
+
+(defn viewing-distance [trees tree]
+  (loop [tree tree
+         trees trees
+         distance 0]
+    (cond
+      (empty? trees)
+      distance
+
+      (>= (first trees) tree)
+      (inc distance)
+
+      :else
+      (recur tree (rest trees) (inc distance)))))
 
 (defn check-height [acc tree] 
   (let [tallest (acc :tallest)]
@@ -19,85 +33,50 @@
           (update :line #(conj % true)))
       (update acc :line #(conj % nil)))))
 
-(defn find-visible [line]
-  (let [f #(reduce check-height {:tallest -1 :line []} %)
-        forwards (:line (f line))
-        backwards (:line (f (reverse line)))
-        combined (map vector forwards (reverse backwards))]
-    (map #(some identity % ) combined)))
-
-(defn rotate [forest]
-  (apply map vector forest))
-
-(defn combine-lines [lines]
-  (let [[horizontal vertical] lines]
-    (->> (map vector horizontal vertical)
-         (map #(some identity %)))))
-
-(defn find-visible-trees [lines] 
-  (let [horizontal (map find-visible lines)
-        vertical (map find-visible (rotate lines))
-        combined (map vector horizontal (rotate vertical))]
-    (map combine-lines combined)))
-
-(defn count-visible-trees-in-line [line]
-  (count-where identity line))
-
-(defn count-visible-trees [visible-trees]
-  (-> (map count-visible-trees-in-line visible-trees) 
-      sum))
-
-(defn part-one []
-  (let [input (->> (get-input-lines "day8")
-                   (map parse-line))]
-    (-> input
-        find-visible-trees
-        count-visible-trees)))
-
-(defn viewing-distance [trees tree]
-  (loop [tree tree
-         trees trees
-         distance 0]
-    (cond 
-      (empty? trees)
-      distance
-      
-      (>= (first trees) tree)
-      (inc distance)
-      
-      :else
-      (recur tree (rest trees) (inc distance)))))
-
-
 (defn find-viewing-distance [acc tree]
   (let [distance (viewing-distance (acc :trees) tree)]
     (-> acc
         (update :line #(conj % distance))
         (update :trees #(concat [tree] %)))))
 
-(defn find-viewing-distances-for-line [line]
-  (let [f #(reduce find-viewing-distance {:line [] :trees []} %)
-        forwards (:line (f line))
-        backwards (:line (f (reverse line)))
-        combined (map vector forwards (reverse backwards))]
-    (map #(apply * %) combined)))
+(defn calculate-line-f [calculate-f merge-f]
+  (fn [line]
+    (let [forwards (:line (calculate-f line))
+          backwards (:line (calculate-f (reverse line)))
+          combined (map vector forwards (reverse backwards))]
+      (map merge-f combined))))
 
-(defn combine-distances [line]
-  (let [ [horizontal vertical] line]
-       (->> (map vector horizontal vertical)
-            (map #(apply * %)))))
+(defn combine-f [f]
+  (fn [line]
+    (let [[horizontal vertical] line]
+      (->> (map vector horizontal vertical)
+           (map f)))))
 
-(defn find-viewing-distances [forest]
-  (let [horizontal (map find-viewing-distances-for-line forest)
-        vertical (map find-viewing-distances-for-line (rotate forest))
+(defn calculate-forest [lines calculate-f merge-f result-f]
+  (let [calculate-f (calculate-line-f calculate-f merge-f)
+        combine-f (combine-f merge-f)
+        horizontal (map calculate-f lines)
+        vertical (map  calculate-f (rotate lines))
         combined (map vector horizontal (rotate vertical))]
-    (->> (map combine-distances combined)
-         (map #(apply max %))
-         (apply max))))
+    (as-> (map combine-f combined) forest
+      (flatten forest)
+      (result-f forest))))
+
+(defn part-one []
+  (let [calculate-f #(reduce check-height {:tallest -1 :line []} %)
+        merge-f #(some identity %)
+        result-f #(count-where true? %)] 
+      (calculate-forest forest calculate-f merge-f result-f)))
 
 
 (defn part-two []
-  (find-viewing-distances (map parse-line (get-input-lines "day8"))))
+  (let [calculate-f #(reduce find-viewing-distance {:line [] :trees []} %)
+        merge-f #(apply * %)
+        result-f #(apply max %)]
+    (calculate-forest forest calculate-f merge-f result-f)))
 
-(part-two)
+(comment
+  (part-one) 
+  (part-two)
+  )
 
